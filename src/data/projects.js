bab32366a0df6f261d32498143d090cabd1d17e2
Leo Lou <leo@triforcecreative.com>
2025-09-10 11:37:44 -0700

    added temporary images to projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,29 +1,33 @@
+import IdleCapitalistImg from '../assets/anime-idle-tycoon.jpg';
+import QuizMeImg from '../assets/cute-anime-girl-glasses.jpg';
+import LootBoxSimulatorImg from '../assets/anime-girl-gift.jpg';
+
 const portfoliosData = [
   {
     slug: 'idle-capitalist',
     title: 'The Idle Capitalist',
     description: "Inspired by the game 'Adventure Capitalist', this is a game where you can build your own business empire. The game is still in development.",
-    image: "",
+    image: IdleCapitalistImg,
     liveUrl: "",
     sourceUrl: "",
-    launchdate: "2026-03-25T23:59:59",
+    launchdate: "2026-06-25T23:59:59",
     details: [],
   },
   {
     slug: 'quiz-me',
     title: 'Quiz Me',
     description: 'This is a quiz app that allows you to create and take quizzes. You can create your own quizzes and share them with your friends.',
-    image: "",
+    image: QuizMeImg,
     liveUrl: "",
     sourceUrl: "",
-    launchdate: "2026-05-11T23:59:59",
+    launchdate: "2026-03-11T23:59:59",
     details: [],
   },
   {
     slug: 'loot-box-simulator',
     title: 'Loot Box Simulator',
     description: 'This is a simulator that simulates the real odds of popular loot boxes, mystery boxes, and gacha. It is a simple app in which you can open boxes and see what you get, see the odds, and track your spending.',
-    image: "",
+    image: LootBoxSimulatorImg,
     liveUrl: "",
     sourceUrl: "",
     launchdate: "2025-11-15T23:59:59",
